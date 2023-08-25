bd06c8c3be3dd56e978d9e0aad0fb67ba80bc674
yuvi-bhatnagar <108606754+yuvi-bhatnagar@users.noreply.github.com>
2023-08-25 07:03:51 +0530

    eCommerce

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
 module.exports.init = async function () {
-  await mongoose.connect("mongodb+srv://yuvrajbhatnagar36:yuvrajbhatnagar36@cluster0.iq1uuxm.mongodb.net/ECommerce?retryWrites=true&w=majority");
+  await mongoose.connect("mongodb+srv://<username>:<password>@cluster0.iq1uuxm.mongodb.net/ECommerce?retryWrites=true&w=majority");
   console.log("Server is connected to database");
 };
